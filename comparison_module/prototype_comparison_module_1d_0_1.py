747a609ee53c3da355328cf7f33978361c553b22
creaneroDIAS_morrigan <creanero@morrigan.ap.dias.ie>
2018-03-07 15:58:47 +0000

    cleaned up variable names

--- comparison_module/prototype_comparison_module_1d_0_1.py
+++ comparison_module/prototype_comparison_module_1d_0_1.py
@@ -13,9 +13,9 @@ from scipy.stats.stats import pearsonr
 
 def read_dreambeam_csv(in_file):
     out_df=pd.read_csv(in_file,\
-                        converters={u' J11':complex,u' J12':complex,\
-                                    u' J21':complex,u' J22':complex}, \
-                        parse_dates=[u'Time'])  
+                        converters={'J11':complex,'J12':complex,\
+                                    'J21':complex,'J22':complex}, \
+                        parse_dates=['Time'], skipinitialspace=True)  
     return out_df
 
 if __name__ == "__main__":
@@ -31,19 +31,19 @@ if __name__ == "__main__":
     scope_df=read_dreambeam_csv(in_file_scope)
     
     #merges the two datagrames using time and frequency
-    merge_df=pd.merge(model_df,scope_df,on=(u'Time',u' Freq'),suffixes=('_model','_scope'))
+    merge_df=pd.merge(model_df,scope_df,on=('Time','Freq'),suffixes=('_model','_scope'))
     
     #calculates the p-channel intensity as per DreamBeam for both model and scope
-    p_ch_model = np.abs(merge_df[u' J11_model'])**2+np.abs(merge_df[u' J12_model'])**2
-    p_ch_scope = np.abs(merge_df[u' J11_scope'])**2+np.abs(merge_df[u' J12_scope'])**2
+    merge_df['p_ch_model'] = np.abs(merge_df['J11_model'])**2+np.abs(merge_df['J12_model'])**2
+    merge_df['p_ch_scope'] = np.abs(merge_df['J11_scope'])**2+np.abs(merge_df['J12_scope'])**2
     #calculates the difference between model and scope
-    p_ch_diff = p_ch_model - p_ch_scope
+    merge_df['p_ch_diff'] = merge_df['p_ch_model'] - merge_df['p_ch_scope']
     
     #calculates the q-channel intensity as per DreamBeam for both model and scope
-    q_ch_model = np.abs(merge_df[u' J21_model'])**2+np.abs(merge_df[u' J22_model'])**2
-    q_ch_scope = np.abs(merge_df[u' J21_scope'])**2+np.abs(merge_df[u' J22_scope'])**2
+    merge_df['q_ch_model'] = np.abs(merge_df['J21_model'])**2+np.abs(merge_df['J22_model'])**2
+    merge_df['q_ch_scope'] = np.abs(merge_df['J21_scope'])**2+np.abs(merge_df['J22_scope'])**2
     #calculates the difference between model and scope
-    q_ch_diff = q_ch_model - q_ch_scope
+    merge_df['q_ch_diff'] = merge_df['q_ch_model'] - merge_df['q_ch_scope']
     
     #creates a two part plot of the values of model and scope
     #part one: plots the model and scope values for p-channel against time
@@ -51,16 +51,16 @@ if __name__ == "__main__":
     plt.title("Plot of the values in p- and q-channels over time")
     plt.subplot(211)
     plt.title("p-channel")
-    plt.plot(merge_df[u'Time'],p_ch_model,label='model',color='orangered')
-    plt.plot(merge_df[u'Time'],p_ch_scope,label='scope',color='darkred')
+    plt.plot(merge_df['Time'],merge_df['p_ch_model'],label='model',color='orangered')
+    plt.plot(merge_df['Time'],merge_df['p_ch_scope'],label='scope',color='darkred')
     plt.legend(frameon=False)
     plt.xticks([])
     
     #part two: plots the model and scope values for q-channel against time
     plt.subplot(212)
     plt.title("q-channel")
-    plt.plot(merge_df[u'Time'],q_ch_model,label='model',color='limegreen')
-    plt.plot(merge_df[u'Time'],q_ch_scope,label='scope',color='darkgreen')
+    plt.plot(merge_df['Time'],merge_df['q_ch_model'],label='model',color='limegreen')
+    plt.plot(merge_df['Time'],merge_df['q_ch_scope'],label='scope',color='darkgreen')
     plt.xticks(rotation=90)
     plt.legend(frameon=False)
     plt.xlabel('Time')
@@ -72,8 +72,8 @@ if __name__ == "__main__":
     
     
     #plots the differences in p-channel and q-channel values over time
-    plt.plot(merge_df[u'Time'],p_ch_diff,label=r'$\Delta p$',color='red')
-    plt.plot(merge_df[u'Time'],q_ch_diff,label=r'$\Delta q$',color='green')
+    plt.plot(merge_df['Time'],merge_df['p_ch_diff'],label=r'$\Delta p$',color='red')
+    plt.plot(merge_df['Time'],merge_df['q_ch_diff'],label=r'$\Delta q$',color='green')
     plt.xticks(rotation=90)
     plt.title("Plot of the differences in p- and q-channels over time")
     plt.legend(frameon=False)
@@ -81,11 +81,11 @@ if __name__ == "__main__":
     plt.show()
     
     #calculates the pearson correlation coefficient between scope and model
-    p_corr=pearsonr(p_ch_model,p_ch_scope)[0]
-    q_corr=pearsonr(q_ch_model,q_ch_scope)[0]
+    p_corr=pearsonr(merge_df['p_ch_model'],merge_df['p_ch_scope'])[0]
+    q_corr=pearsonr(merge_df['q_ch_model'],merge_df['q_ch_scope'])[0]
     print("\nThe P-channel correlation is %f\nThe Q-channel correlation is %f"%(p_corr,q_corr))
     
     #calculates the root mean squared error between scope and model
-    p_rmse=np.mean(p_ch_diff**2)**0.5
-    q_rmse=np.mean(q_ch_diff**2)**0.5
+    p_rmse=np.mean(merge_df['p_ch_diff']**2)**0.5
+    q_rmse=np.mean(merge_df['q_ch_diff']**2)**0.5
     print("\nThe P-channel RMSE is %f\nThe Q-channel RMSE is %f"%(p_rmse,q_rmse))
