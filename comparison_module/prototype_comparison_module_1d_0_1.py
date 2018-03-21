499abc6003f89627c3e43109267eaab09c600763
creaneroDIAS_morrigan <creanero@morrigan.ap.dias.ie>
2018-03-21 08:02:50 +0000

    Added parser functionality to the comparison module

--- comparison_module/prototype_comparison_module_1d_0_1.py
+++ comparison_module/prototype_comparison_module_1d_0_1.py
@@ -379,15 +379,57 @@ def colour_models(colour_id):
     else:
         print("Warning: Colour incorrectly specified.  Defaulting to Black")
         return ('black')    
+
+def beam_arg_parser():
+    parser = argparse.ArgumentParser()
+    
+    #creates a group for the model filename
+    group_model = parser.add_mutually_exclusive_group()
+    
+    #gives positional and optional ways of providing the model data 
+    group_model.add_argument("model_p",nargs='?', default=None, 
+                             help="The file containing the data from the model (Usually DreamBeam)")
+    group_model.add_argument("--model","-m", 
+                             help="Alternative way of specifying the file containing the data from the model")
+    
+    
+    #creates a group for the scope filename
+    group_scope = parser.add_mutually_exclusive_group()
+    
+    #gives positional and optional ways of providing the scope data 
+    group_scope.add_argument("scope_p",nargs='?', default=None, 
+                             help="The file containing the observed data from the telescope")
+    group_scope.add_argument("--scope","-s", 
+                             help="Alternative way of specifying the file containing the observed data from the telescope")
+    
+    
+    #passes these arguments to a unified variable
+    args = parser.parse_args()
+    
+    #outputs the filename for the model to a returnable variable
+    if args.model_p != None:
+        in_file_model=args.model_p
+    elif args.model != None:
+        in_file_model=args.model
+    else:
+        in_file_model=raw_input("No model filename specified:\n"
+                                "Please enter the model filename:\n")
+    
+    
+    #outputs the filename for the scope to a returnable variable
+    if args.scope_p != None:
+        in_file_scope=args.scope_p
+    elif args.scope != None:
+        in_file_scope=args.scope
+    else:
+        in_file_scope=raw_input("No filename specified for observed data from the telescope:\n"
+                                "Please enter the telescope filename:\n")
+    
+    return(in_file_model,in_file_scope)
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    #put the arguments in here
-    args=parser.parse_args()
-    
-    #User input the filenames - probably want to parameterise this.
-    in_file_model=raw_input("Please enter the model filename:\n")#"~/outputs/test/dreamBeam/2018-03-05/SE607_1d_160M.csv"
-    in_file_scope=raw_input("Please enter the scope filename:\n")#"~/outputs/test/dreamBeam/2018-03-05/IE613_1d_160M.csv"
+    #gets the command line arguments for the scope and model filename
+    in_file_model,in_file_scope=beam_arg_parser()
     
     #read in the csv files from DreamBeam and format them correctly
     #want to modularise this
