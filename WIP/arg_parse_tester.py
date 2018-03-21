6ac2a48bc1b75408e8128bf1b3a2569600075506
creaneroDIAS_morrigan <creanero@morrigan.ap.dias.ie>
2018-03-21 07:44:18 +0000

    Arg Parsing function added

--- WIP/arg_parse_tester.py
+++ WIP/arg_parse_tester.py
@@ -6,25 +6,44 @@ Created on Tue Mar 20 16:22:34 2018
 """
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("model_p",nargs='?', default=None, help="echo the string you use here")
-parser.add_argument("scope_p",nargs='?', default=None, help="echo the string you use here")
-parser.add_argument("--scope","-s", help="echo the string you use here")
-parser.add_argument("--model","-m", help="echo the string you use here")
-args = parser.parse_args()
 
-if args.model_p != None:
-    in_file_model=args.model_p
-elif args.model != None:
-    in_file_model=args.model
-else:
-    in_file_model=raw_input("No model")
-
-print(in_file_model)
-
-if args.scope_p != None:
-    print (args.scope_p)
-elif args.scope != None:
-    print (args.scope)
-else:
-    print("test scope")
+def beam_arg_parser():
+    parser = argparse.ArgumentParser()
+    group_model = parser.add_mutually_exclusive_group()
+    
+    
+    
+    group_model.add_argument("model_p",nargs='?', default=None, 
+                             help="The file containing the data from the model (Usually DreamBeam)")
+    group_model.add_argument("--model","-m", 
+                             help="Alternative way of specifying the file containing the data from the model")
+    
+    group_scope = parser.add_mutually_exclusive_group()
+    group_scope.add_argument("scope_p",nargs='?', default=None, 
+                             help="The file containing the observed data from the telescope")
+    group_scope.add_argument("--scope","-s", 
+                             help="Alternative way of specifying the file containing the observed data from the telescope")
+    
+    args = parser.parse_args()
+    
+    if args.model_p != None:
+        in_file_model=args.model_p
+    elif args.model != None:
+        in_file_model=args.model
+    else:
+        in_file_model=raw_input("No model filename specified:\n"
+                                "Please enter the model filename:\n")
+    
+    print(in_file_model)
+    
+    if args.scope_p != None:
+        in_file_scope=args.scope_p
+    elif args.scope != None:
+        in_file_scope=args.scope
+    else:
+        in_file_scope=raw_input("No filename specified for observed data from the telescope:\n"
+                                "Please enter the telescope filename:\n")
+    
+    print(in_file_scope)
+    
+beam_arg_parser()
