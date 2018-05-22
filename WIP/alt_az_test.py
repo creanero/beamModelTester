233c81676c7ef93d5afe30bd3c92fdc34f3d8da9
creanero <oisin.creaner@dit.ie>
2018-05-22 10:16:04 +0100

    added tweaks to wip versions.

--- WIP/alt_az_test.py
+++ WIP/alt_az_test.py
@@ -5,6 +5,8 @@ from: convert ra dec to alt az with astropy.py
 https://gist.github.com/dokeeffe/18857db66dbabc14679c20a8560e2cd6
 
 Used experimentally for alt-az calculations
+
+Modified by: Oisin Creaner
 """
 
 
@@ -18,8 +20,15 @@ from astropy.coordinates import AltAz
 #from google maps 57.398743, 11.929636 coordinates for SE607 LBA
 #from google maps 57.398784, 11.930933 coordinates for SE607 HBA
 observing_location = EarthLocation(lat='53.094676', lon='-7.921566', height=150*u.m)  
-observing_time = Time('2018-03-16 11:49:21')  
-aa = AltAz(location=observing_location, obstime=observing_time)
+#observing_time = Time('2018-03-16 11:49:21')  
+#aa = AltAz(location=observing_location, obstime=observing_time)
 
 coord = SkyCoord('23h23m26s', '58d48m')
-coord.transform_to(aa)
+#coord.transform_to(aa)
+
+time_set = Time(list(merge_df.Time))
+aa_set= AltAz(location=observing_location, obstime=time_set)
+coord_set=coord.transform_to(aa_set)
+
+merge_df['alt'] = coord_set.alt
+merge_df['az'] = coord_set.az
--- WIP/clim_test.py
+++ WIP/clim_test.py
@@ -4,11 +4,17 @@ Created on Tue May 15 17:43:16 2018
 
 @author: User
 """
+
+key = 'xx'
+
+source = 'scope'
+
+
 plt.figure()
 plt.subplot(211)
-plt.tripcolor(merge_df.d_Time,merge_df.Freq,plottable(merge_df[key+'_'+source]), cmap=plt.get_cmap(colour_models(key+'s')))
-plt.clim(0,np.percentile(plottable(merge_df[key+'_'+source]),95))
+plt.tripcolor(merge_df.alt,merge_df.Freq,plottable(merge_df[key+'_'+source]), cmap=plt.get_cmap(colour_models(key+'s')))
+plt.clim(np.percentile(plottable(merge_df[key+'_'+source]),5),np.percentile(plottable(merge_df[key+'_'+source]),95))
 plt.subplot(212)
-plt.plot(merge_df.d_Time,merge_df.az,'mo')
-plt.plot(merge_df.d_Time,merge_df.alt,'k+')
+#plt.plot(merge_df.d_Time,merge_df.az,color="magenta", marker=".", linestyle="None")
+plt.plot(merge_df.alt,merge_df.az_360,color="magenta", marker=".", linestyle="None")
 plt.show()
