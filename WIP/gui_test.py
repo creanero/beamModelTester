1cbfd4768957b47a008709784294234ffc67a8fc
creanero <oisin.creaner@dit.ie>
2018-11-14 10:54:03 +0000

    basic window

---
+++ WIP/gui_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 10:48:33 2018
+
+@author: User
+"""
+
+#!/usr/bin/python
+
+import tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
